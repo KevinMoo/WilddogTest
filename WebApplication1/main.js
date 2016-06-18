e46084c1afd51c9f577bb5dfe7db483e9b8bed8a
mo <1731550497@qq.com>
2016-06-18 14:17:54 +0800

    first

---
+++ WebApplication1/main.js
@@ -0,0 +1,48 @@
+﻿// 此处显示你的代码!
+var wilddogConfig = "https://mo123456789.wilddogio.com/mytest"
+var ref = new Wilddog(wilddogConfig);
+ref.set({
+    "name": "jack",
+    "age": 20,
+    "ismarry": false,
+    "from": "USA",
+    "parent": {
+        "fatther": {
+            "name": "di",
+            "age": 40
+        }, "mather":
+        {
+            "name": "lucy",
+            "age": 40
+        }
+    }
+});
+
+ref.child("parent/fatther/name").once('value', function (datasnapshot, error) {
+    if (error == null) {
+        document.getElementById('con').innerHTML = datasnapshot.val();
+    }
+});
+
+
+function testOnClick() {
+    ref.child("parent/fatther/name").set(new Date().getTime());
+}
+
+function pushOnClick() {
+    ref.push("pushValue");
+}
+
+function testJson() {
+    var s = '{"name":"moshuchao","age":20}';
+    var obj = $.evalJSON(s);
+    //var obj = JSON.parse(s);
+    var s1 = $.toJSON(obj);
+    alert(obj.name);
+    alert(s1);
+}
+
+function updateOnClick() {
+    ref.child("parent/fatther/name").update("Mo");
+}
+
